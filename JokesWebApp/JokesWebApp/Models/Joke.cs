d5691d81a53b6ef70702262e83268cb9a05fe44d
TakeshiYoshikawa <takeshiy096@gmail.com>
2022-07-12 00:57:56 -0300

    Initial commit

---
+++ JokesWebApp/JokesWebApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; } = string.Empty;
+        public string JokeAnswer { get; set; } = string.Empty;
+
+        public Joke()
+        {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # JokesWebApp
+
+# Build database
+- Open Package Manager Console
+- Run these commands:
+
+```
+Add-Migration <Description>
+Update-Database
+```
